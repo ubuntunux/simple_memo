967c22245650349423d8741f44817aa3141115b3
ubuntunux <ubuntunux@gmail.com>
2022-12-15 02:38:41 +0900

    update tab bar

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -25,22 +26,53 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> with TickerProviderStateMixin {
+  int _selectedIndex = 0;
   int _counter = 0;
-
-  late TabController _tabController;
+  final List<Tab> _tabs = List<Tab>.empty(growable: true);
+  final List<Widget> _tabViews = List<Widget>.empty(growable: true);
+  TabController? _tabController;
 
   @override
   void initState() {
-    _tabController = TabController(
-      length: 2,
-      vsync: this,  // vsync에 this 형태로 전달해야 애니메이션이 정상 처리됨
-    );
+    _tabs.clear();
+    _tabViews.clear();
+    _tabController = TabController(length: 0, vsync: this);
     super.initState();
   }
 
-  void incrementCounter() {
+  @override
+  void dispose() {
+    _tabController?.dispose();
+    super.dispose();
+  }
+
+  void addTab() {
     setState(() {
-      _counter+=2;
+      _tabs.add(Tab(
+        text: "Tab $_counter",
+      ));
+
+      _tabViews.add(
+          Container(
+            color: const Color(0xFF111111),
+            alignment: Alignment.center,
+            child: Text(
+              'TabView $_counter',
+              style: const TextStyle(
+                fontSize: 30,
+              ),
+            ),
+          )
+      );
+
+      _tabController?.dispose();
+      _tabController = TabController(
+        length: _tabs.length,
+        initialIndex: _counter,
+        vsync: this,
+      );
+
+      _counter++;
     });
   }
 
@@ -67,62 +99,39 @@ class _MyHomePageState extends State<MyHomePage> with TickerProviderStateMixin {
                 border: Border.all(),
               ),
               child: TabBar(
-                tabs: [
-                  Container(
-                    height: 80,
-                    alignment: Alignment.center,
-                    child: const Text(
-                      'Tab1',
-                    ),
-                  ),
-                  Container(
-                    height: 80,
-                    alignment: Alignment.center,
-                    child: const Text(
-                      'Tab2',
-                    ),
-                  ),
-                ],
+                splashBorderRadius: BorderRadius.circular(50),
+                isScrollable: true,
+                tabs: _tabs,
+                indicatorWeight: 10,
+                indicatorSize: TabBarIndicatorSize.label,
                 indicator: const BoxDecoration(
                   color: Colors.grey,
                 ),
                 labelColor: Colors.white,
                 unselectedLabelColor: Colors.black,
                 controller: _tabController,
+                onTap: (int index) {
+                  setState(() {
+                    _selectedIndex = index;
+                  });
+
+                  if (kDebugMode) {
+                    print('Tab $index is tapped');
+                  }
+                },
               ),
             ),
             Expanded(
               child: TabBarView(
                 controller: _tabController,
-                children: [
-                  Container(
-                    color: const Color(0xFF111111),
-                    alignment: Alignment.center,
-                    child: const Text(
-                      'Tab1 View',
-                      style: TextStyle(
-                        fontSize: 30,
-                      ),
-                    ),
-                  ),
-                  Container(
-                    color: const Color(0xFF111111),
-                    alignment: Alignment.center,
-                    child: const Text(
-                      'Tab2 View',
-                      style: TextStyle(
-                        fontSize: 30,
-                      ),
-                    ),
-                  ),
-                ],
+                children: _tabViews,
               ),
             ),
           ],
         ),
       ),
       floatingActionButton: FloatingActionButton(
-        onPressed: incrementCounter,
+        onPressed: addTab,
         tooltip: 'Increment',
         child: const Icon(Icons.add),
       ), // This trailing comma makes auto-formatting nicer for build methods.
