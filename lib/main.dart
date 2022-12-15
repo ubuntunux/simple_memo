95926357790b4fcbb4d90042c79978c5804fc1d7
ubuntunux <ubuntunux@gmail.com>
2022-12-16 02:30:53 +0900

    update

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,11 @@
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 
+enum ContextMenu {
+  addCategory,
+  removeCategory,
+}
+
 void main() {
   runApp(const MyApp());
 }
@@ -26,9 +31,8 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> with TickerProviderStateMixin {
-  int _selectedIndex = 0;
-  int _counter = 0;
-  final List<Tab> _tabs = List<Tab>.empty(growable: true);
+  int _selectedCategoryIndex = 0;
+  final List<Widget> _tabs = List<Widget>.empty(growable: true);
   final List<Widget> _tabViews = List<Widget>.empty(growable: true);
   TabController? _tabController;
 
@@ -48,8 +52,10 @@ class _MyHomePageState extends State<MyHomePage> with TickerProviderStateMixin {
 
   void addTab() {
     setState(() {
+      int index = _tabs.length;
+
       _tabs.add(Tab(
-        text: "Tab $_counter",
+        text: "Tab $index",
       ));
 
       _tabViews.add(
@@ -57,7 +63,7 @@ class _MyHomePageState extends State<MyHomePage> with TickerProviderStateMixin {
             color: const Color(0xFF111111),
             alignment: Alignment.center,
             child: Text(
-              'TabView $_counter',
+              'TabView $index',
               style: const TextStyle(
                 fontSize: 30,
               ),
@@ -68,12 +74,39 @@ class _MyHomePageState extends State<MyHomePage> with TickerProviderStateMixin {
       _tabController?.dispose();
       _tabController = TabController(
         length: _tabs.length,
-        initialIndex: _counter,
+        initialIndex: index,
         vsync: this,
       );
-
-      _counter++;
+      _selectedCategoryIndex = index;
     });
+  }
+
+  void removeTab() {
+    setState(() {
+      if(1 < _tabs.length) {
+        _tabs.removeAt(_selectedCategoryIndex);
+        _tabViews.removeAt(_selectedCategoryIndex);
+
+        if (0 < _selectedCategoryIndex) {
+          _selectedCategoryIndex -= 1;
+        }
+
+        _tabController?.dispose();
+        _tabController = TabController(
+          length: _tabs.length,
+          initialIndex: _selectedCategoryIndex,
+          vsync: this,
+        );
+      }
+    });
+  }
+
+  void selectContextMenu(ContextMenu menu) {
+    if (ContextMenu.addCategory == menu) {
+      addTab();
+    } else if (ContextMenu.removeCategory == menu) {
+      removeTab();
+    }
   }
 
   @override
@@ -81,43 +114,70 @@ class _MyHomePageState extends State<MyHomePage> with TickerProviderStateMixin {
     return Scaffold(
       appBar: AppBar(
         title: Text(widget.title),
+        actions: <Widget>[
+          PopupMenuButton(
+            onSelected: (ContextMenu menu) { selectContextMenu(menu); },
+            padding: EdgeInsets.zero,
+            itemBuilder: (BuildContext context) {
+              return [
+                const PopupMenuItem<ContextMenu>(
+                  value: ContextMenu.addCategory,
+                  child: Text("Add Category"),
+                ),
+                const PopupMenuItem<ContextMenu>(
+                  value: ContextMenu.removeCategory,
+                  child: Text("Remove Category"),
+                ),
+              ];
+            },
+          )
+        ],
       ),
       body: Center(
         child: Column(
           crossAxisAlignment: CrossAxisAlignment.stretch,
           children: <Widget>[
-            const Text(
-                '이걸 클릭하시오.',
-                textAlign: TextAlign.left
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
-            ),
             Container(
               decoration: BoxDecoration(
                 border: Border.all(),
               ),
               child: TabBar(
-                splashBorderRadius: BorderRadius.circular(50),
                 isScrollable: true,
                 tabs: _tabs,
-                indicatorWeight: 10,
-                indicatorSize: TabBarIndicatorSize.label,
                 indicator: const BoxDecoration(
-                  color: Colors.grey,
+                  color: Color(0xFF555555),
                 ),
                 labelColor: Colors.white,
                 unselectedLabelColor: Colors.black,
                 controller: _tabController,
                 onTap: (int index) {
                   setState(() {
-                    _selectedIndex = index;
+                    _selectedCategoryIndex = index;
                   });
 
-                  if (kDebugMode) {
-                    print('Tab $index is tapped');
-                  }
+                  showDialog(
+                    context: context,
+                    builder: (context) {
+                      return Dialog(
+                        shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(40)),
+                        elevation: 16,
+                        child: ListView(
+                          shrinkWrap: true,
+                          children: <Widget>[
+                            const SizedBox(height: 20),
+                            const Center(child: Text('Leaderboard')),
+                            const SizedBox(height: 20),
+                            buildRow('assets/choc.png', 'Name 1', 1000),
+                            buildRow('assets/choc.png', 'Name 2', 2000),
+                            buildRow('assets/choc.png', 'Name 3', 3000),
+                            buildRow('assets/choc.png', 'Name 4', 4000),
+                            buildRow('assets/choc.png', 'Name 5', 5000),
+                            buildRow('assets/choc.png', 'Name 6', 6000),
+                          ],
+                        ),
+                      );
+                    },
+                  );  // showDialog
                 },
               ),
             ),
@@ -137,4 +197,30 @@ class _MyHomePageState extends State<MyHomePage> with TickerProviderStateMixin {
       ), // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
+}
+
+Widget buildRow(String imageAsset, String name, double score) {
+  return Padding(
+    padding: const EdgeInsets.symmetric(horizontal: 20.0),
+    child: Column(
+      children: <Widget>[
+        const SizedBox(height: 12),
+        Container(height: 2, color: Colors.redAccent),
+        const SizedBox(height: 12),
+        Row(
+          children: <Widget>[
+            //CircleAvatar(backgroundImage: AssetImage(imageAsset)),
+            const SizedBox(width: 12),
+            Text(name),
+            const Spacer(),
+            Container(
+              decoration: BoxDecoration(color: Colors.yellow[900], borderRadius: BorderRadius.circular(20)),
+              padding: const EdgeInsets.symmetric(vertical: 8, horizontal: 20),
+              child: Text('$score'),
+            ),
+          ],
+        ),
+      ],
+    ),
+  );
 }
